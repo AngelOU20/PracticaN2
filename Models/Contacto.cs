9c0c3e974db1c0ea74813c836b2144e0dd1e803f
Julio Ucharima <julio_ucharima@usmp.pe>
2021-04-27 18:53:08 -0500

    Creacion de la clase contacto en models

---
+++ Models/Contacto.cs
@@ -0,0 +1,19 @@
+
+namespace PracticaN2.Models
+{
+    public class Contacto
+    {
+       
+        public int id{get;set;}
+
+       
+        public string Nombre{get;set;}
+
+        
+        public string Apellido{get;set;}
+        public string Motivo{get;set;}
+
+        
+        public string Mensaje{get;set;}
+    }
+}
